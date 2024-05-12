d5b82decb9a615fbdc268ae17af8bae4125525e9
Kumar <ganja.nagakumar@gmail.com>
2024-05-12 21:10:27 +0530

    inital

---
+++ GNKiOSPods/GNKiOSPodsApp.swift
@@ -0,0 +1,17 @@
+//
+//  GNKiOSPodsApp.swift
+//  GNKiOSPods
+//
+//  Created by NagaKumar Ganja on 12/05/24.
+//
+
+import SwiftUI
+
+@main
+struct GNKiOSPodsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
